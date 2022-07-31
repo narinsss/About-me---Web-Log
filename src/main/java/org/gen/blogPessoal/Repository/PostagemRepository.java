712c5a9ed6c215af6be4b2f70a2472733567a115
narinsss <maarib03@gmail.com>
2022-07-31 20:56:24 -0300

    fc

---
+++ src/main/java/org/gen/blogPessoal/Repository/PostagemRepository.java
@@ -0,0 +1,13 @@
+package org.gen.blogPessoal.Repository;
+
+import java.util.List;
+
+import org.gen.blogPessoal.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+
+}
