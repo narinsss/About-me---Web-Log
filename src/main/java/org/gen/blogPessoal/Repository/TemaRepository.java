ac8d4c46646a5deda60a32f0acad3666a91dd37d
narinsss <maarib03@gmail.com>
2022-08-02 13:54:48 -0300

    Classe repository da class Tema criado

---
+++ src/main/java/org/gen/blogPessoal/Repository/TemaRepository.java
@@ -0,0 +1,11 @@
+package org.gen.blogPessoal.Repository;
+
+import java.util.List;
+
+import org.gen.blogPessoal.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemaRepository extends JpaRepository<Tema, Long>{
+	public List<Tema> findAllByCategoriaContainingIgnoreCase(String categoria);
+
+}
